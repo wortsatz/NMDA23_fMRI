d072133894324020e6964a3b9a7a9fd2d1c47a20
aasmolova <120099867+aasmolova@users.noreply.github.com>
2023-05-19 22:03:16 +0200

    Update auditory_spm12_batch.m

--- auditory_spm12_batch.m
+++ auditory_spm12_batch.m
@@ -114,81 +114,25 @@ matlabbatch{2}.spm.spatial.coreg.estimate.source = cellstr(a);
 % matlabbatch{2}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
 
 
+%--------------------------------------------------------------------------
 % Segment
-%--------------------------------------------------------------------------
-
 % initialize the parameters for the segmentation
-
-matlabbatch{3}.spm.spatial.preproc.channel.vols  = cellstr(a);
-% matlabbatch{3}.spm.spatial.preproc.channel.biasreg = 0.001;
-% matlabbatch{3}.spm.spatial.preproc.channel.biasfwhm = 60;
-matlabbatch{3}.spm.spatial.preproc.channel.write = [0 1];
-% matlabbatch{3}.spm.spatial.preproc.tissue(1).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,1'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(1).ngaus = 1;
-% matlabbatch{3}.spm.spatial.preproc.tissue(1).native = [1 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(1).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(2).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,2'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(2).ngaus = 1;
-% matlabbatch{3}.spm.spatial.preproc.tissue(2).native = [1 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(2).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(3).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,3'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(3).ngaus = 2;
-% matlabbatch{3}.spm.spatial.preproc.tissue(3).native = [1 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(3).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(4).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,4'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(4).ngaus = 3;
-% matlabbatch{3}.spm.spatial.preproc.tissue(4).native = [1 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(4).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(5).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,5'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(5).ngaus = 4;
-% matlabbatch{3}.spm.spatial.preproc.tissue(5).native = [1 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(5).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(6).tpm = {'/Users/lp1/Documents/spm12/tpm/TPM.nii,6'};
-% matlabbatch{3}.spm.spatial.preproc.tissue(6).ngaus = 2;
-% matlabbatch{3}.spm.spatial.preproc.tissue(6).native = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.tissue(6).warped = [0 0];
-% matlabbatch{3}.spm.spatial.preproc.warp.mrf = 1;
-% matlabbatch{3}.spm.spatial.preproc.warp.cleanup = 1;
-% matlabbatch{3}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
-% matlabbatch{3}.spm.spatial.preproc.warp.affreg = 'mni';
-% matlabbatch{3}.spm.spatial.preproc.warp.fwhm = 0;
-% matlabbatch{3}.spm.spatial.preproc.warp.samp = 3;
-matlabbatch{3}.spm.spatial.preproc.warp.write = [0 1];
-% matlabbatch{3}.spm.spatial.preproc.warp.vox = NaN;
-% matlabbatch{3}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
-%                                               NaN NaN NaN];
-
+s = segmentation(a);
+matlabbatch{3} = s{3}; 
+%--------------------------------------------------------------------------
 % Normalise: Write
-%--------------------------------------------------------------------------
-
 % initialize the parameters for the normalization
-
-% This is the normalization of the functional image 
-matlabbatch{4}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
-matlabbatch{4}.spm.spatial.normalise.write.subj.resample = cellstr(f);
-matlabbatch{4}.spm.spatial.normalise.write.woptions.vox  = [3 3 3];
-
-% matlabbatch{4}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
-%                                                           78 76 85];
-% matlabbatch{4}.spm.spatial.normalise.write.woptions.interp = 4;
-% matlabbatch{4}.spm.spatial.normalise.write.woptions.prefix = 'w';
-
-% This is for the Normalization of the structural image 
-matlabbatch{5}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
-matlabbatch{5}.spm.spatial.normalise.write.subj.resample = cellstr(spm_file(a,'prefix','m','ext','nii'));
-matlabbatch{5}.spm.spatial.normalise.write.woptions.vox  = [1 1 3];
-
-% Smooth
+n = normalization(a, f);
+matlabbatch{4} = n{4};
+matlabbatch{5} = n{5};
 %--------------------------------------------------------------------------
-
+% Smooth
 % initialize the parameters for the smoothing
-
-matlabbatch{6}.spm.spatial.smooth.data = cellstr(spm_file(f,'prefix','w'));
-matlabbatch{6}.spm.spatial.smooth.fwhm = [6 6 6];
-% matlabbatch{6}.spm.spatial.smooth.dtype = 0;
-% matlabbatch{6}.spm.spatial.smooth.im = 0;
-% matlabbatch{6}.spm.spatial.smooth.prefix = 's';
-
+sm = smooting(f);
+matlabbatch{6} = sm{6};
+%--------------------------------------------------------------------------
+%%
+ %Job script 
 spm_jobman('run',matlabbatch);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
