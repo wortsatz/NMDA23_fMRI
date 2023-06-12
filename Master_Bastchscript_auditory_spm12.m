3298b5196bc96b275a15c31979aed51f4cf32dee
Tiantianttli <ttli@bu.edu>
2023-06-12 10:13:39 +0200

    group_and_tutorial
    combined with group solution

--- Master_Bastchscript_auditory_spm12.m
+++ Master_Bastchscript_auditory_spm12.m
@@ -74,6 +74,46 @@ spm_jobman('run',matlabbatch);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% SPATIAL PREPROCESSING
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%######################### auditory tutorial ########################
+% f = spm_select('FPList', fullfile(data_path,'fM00223'), '^f.*\.img$');
+% a = spm_select('FPList', fullfile(data_path,'sM00223'), '^s.*\.img$');
+% 
+% clear matlabbatch
+% 
+% % Realign
+% %--------------------------------------------------------------------------
+% matlabbatch{1}.spm.spatial.realign.estwrite.data = {cellstr(f)};
+% matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [0 1];
+% 
+% % Coregister
+% %--------------------------------------------------------------------------
+% matlabbatch{2}.spm.spatial.coreg.estimate.ref    = cellstr(spm_file(f(1,:),'prefix','mean'));
+% matlabbatch{2}.spm.spatial.coreg.estimate.source = cellstr(a);
+% 
+% % Segment
+% %--------------------------------------------------------------------------
+% matlabbatch{3}.spm.spatial.preproc.channel.vols  = cellstr(a);
+% matlabbatch{3}.spm.spatial.preproc.channel.write = [0 1];
+% matlabbatch{3}.spm.spatial.preproc.warp.write    = [0 1];
+% 
+% % Normalise: Write
+% %--------------------------------------------------------------------------
+% matlabbatch{4}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
+% matlabbatch{4}.spm.spatial.normalise.write.subj.resample = cellstr(f);
+% matlabbatch{4}.spm.spatial.normalise.write.woptions.vox  = [3 3 3];
+% 
+% matlabbatch{5}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
+% matlabbatch{5}.spm.spatial.normalise.write.subj.resample = cellstr(spm_file(a,'prefix','m','ext','nii'));
+% matlabbatch{5}.spm.spatial.normalise.write.woptions.vox  = [1 1 3];
+% 
+% % Smooth
+% %--------------------------------------------------------------------------
+% matlabbatch{6}.spm.spatial.smooth.data = cellstr(spm_file(f,'prefix','w'));
+% matlabbatch{6}.spm.spatial.smooth.fwhm = [6 6 6];
+% 
+% spm_jobman('run',matlabbatch);
+%##################################################  
+
 
 % create variables for the functional data (f) and the structural data (a)
 
