710d58dbda33ef483aab309d48388df5044dab2a
aasmolova <nastyaasmolova@gmail.com>
2023-08-23 19:05:40 +0200

    EEG preprocessing

--- EEG_preprocessing_batch.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'C:\Users\Nastya\Documents\Freie Universitat\EEG_preprocessing_batch_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'EEG');
-spm_jobman('run', jobs, inputs{:});
