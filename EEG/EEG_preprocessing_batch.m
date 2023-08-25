60ee7302a1b619a9d999e8e46f057bd8b5a09c68
Lisa Paulsen <72503842+wortsatz@users.noreply.github.com>
2023-08-25 20:06:28 +0200

    update EEG Script

--- EEG/EEG_preprocessing_batch.m
+++ EEG/EEG_preprocessing_batch.m
@@ -1,7 +1,44 @@
+
+%% ============= NMDA 23 - EEG MASTER BATCH SCRIPT =======================
+
+% this script is the master batch script for EEG
+
+
+%% -------------------- Parameter Changes -------------------------------
+
+% Select which path is the basis Path
+whichComp=1;
+
+if whichComp==1
+    spmPath='/Users/ttli/Documents/spm12/';
+    data_path='/Users/ttli/Documents/GitHub/NMDA23/98_fMRI_data';
+    project_path='/Users/ttli/Documents/GitHub/NMDA23/';
+elseif whichComp==2
+    spmPath='/Users/kubrafatulla/Documents/spm12';
+    data_path='/Users/kubrafatulla/Documents/fMRI/MoAEpilot/';
+    project_path='/Users/kubrafatulla/Documents/GitHub/NMDA23/';
+elseif whichComp==3 
+    spmPath='/Users/lp1/Documents/spm12';
+    data_path='/Users/lp1/Nextcloud/FU/NMDA_data';
+    project_path='/Users/lp1/Documents/GitHub/NMDA23/';
+else
+    spmPath='/Users/Nastya/программы/spm12';
+    data_path='/Users/Nastya/Documents/Freie Universitat/NMDA';
+    project_path='/Users/Nastya/Documents/GitHub/NMDA23/';
+end
+cd(data_path)
+addpath((spmPath))
+addpath(genpath(project_path))
+
+
+
+%% ------------------------- Preprocessing --------------------------------
+
+
 % List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'C:\Users\Nastya\Documents\Freie Universitat\EEG_preprocessing_batch_job.m'};
-jobs = repmat(jobfile, 1, nrun);
+nrun = 1; % enter the number of runs here
+preprocessing_jobfile = {'C:\Users\Nastya\Documents\Freie Universitat\EEG_preprocessing_batch_job.m'};
+jobs = preprocessing_jobfile;
 inputs = cell(0, nrun);
 for crun = 1:nrun
 end
--- EEG/EEG_preprocessing_batch_job.m
+++ EEG/EEG_preprocessing_batch_job.m
@@ -1,63 +1,120 @@
-%-----------------------------------------------------------------------
-% Job saved on 23-Aug-2023 17:12:39 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7771)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.meeg.convert.dataset = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\subject1.bdf'};
-matlabbatch{1}.spm.meeg.convert.mode.continuous.readall = 1;
-matlabbatch{1}.spm.meeg.convert.channels{1}.chanfile = {'C:\Users\Nastya\Documents\Freie Universitat\channelselection.mat'};
-matlabbatch{1}.spm.meeg.convert.outfile = '';
-matlabbatch{1}.spm.meeg.convert.eventpadding = 0;
-matlabbatch{1}.spm.meeg.convert.blocksize = 3276800;
-matlabbatch{1}.spm.meeg.convert.checkboundary = 1;
-matlabbatch{1}.spm.meeg.convert.saveorigheader = 0;
-matlabbatch{1}.spm.meeg.convert.inputformat = 'autodetect';
-matlabbatch{2}.spm.meeg.preproc.montage.D = {'C:\Users\Nastya\Documents\Freie Universitat\spmeeg_subject1.mat'};
-matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.montagefile = {'C:\Users\Nastya\Documents\Freie Universitat\avref_eog_montage.mat'};
-matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.keepothers = 0;
-matlabbatch{2}.spm.meeg.preproc.montage.mode.write.blocksize = 655360;
-matlabbatch{2}.spm.meeg.preproc.montage.mode.write.prefix = 'M';
-matlabbatch{3}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
-matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.eegsens = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\sensors.pol'};
-matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.megmatch.nomatch = 1;
-matlabbatch{4}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
-matlabbatch{4}.spm.meeg.preproc.filter.type = 'butterworth';
-matlabbatch{4}.spm.meeg.preproc.filter.band = 'high';
-matlabbatch{4}.spm.meeg.preproc.filter.freq = 0.1;
-matlabbatch{4}.spm.meeg.preproc.filter.dir = 'twopass';
-matlabbatch{4}.spm.meeg.preproc.filter.order = 5;
-matlabbatch{4}.spm.meeg.preproc.filter.prefix = 'f';
-matlabbatch{5}.spm.meeg.preproc.downsample.D = {'C:\Users\Nastya\Documents\Freie Universitat\fMspmeeg_subject1.mat'};
-matlabbatch{5}.spm.meeg.preproc.downsample.fsample_new = 200;
-matlabbatch{5}.spm.meeg.preproc.downsample.method = 'resample';
-matlabbatch{5}.spm.meeg.preproc.downsample.prefix = 'd';
-matlabbatch{6}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\dfMspmeeg_subject1.mat'};
-matlabbatch{6}.spm.meeg.preproc.filter.type = 'butterworth';
-matlabbatch{6}.spm.meeg.preproc.filter.band = 'low';
-matlabbatch{6}.spm.meeg.preproc.filter.freq = 30;
-matlabbatch{6}.spm.meeg.preproc.filter.dir = 'twopass';
-matlabbatch{6}.spm.meeg.preproc.filter.order = 5;
-matlabbatch{6}.spm.meeg.preproc.filter.prefix = 'f';
-matlabbatch{7}.spm.meeg.preproc.epoch.D = {'C:\Users\Nastya\Documents\Freie Universitat\fdfMspmeeg_subject1.mat'};
-matlabbatch{7}.spm.meeg.preproc.epoch.trialchoice.trlfile = {'C:\Users\Nastya\Documents\Freie Universitat\trialdef.mat'};
-matlabbatch{7}.spm.meeg.preproc.epoch.bc = 1;
-matlabbatch{7}.spm.meeg.preproc.epoch.eventpadding = 0;
-matlabbatch{7}.spm.meeg.preproc.epoch.prefix = 'e';
-matlabbatch{8}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
-matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.channels{1}.chan = 'A14';
-matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.status = 1;
-matlabbatch{9}.spm.meeg.preproc.artefact.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
-matlabbatch{9}.spm.meeg.preproc.artefact.mode = 'mark';
-matlabbatch{9}.spm.meeg.preproc.artefact.badchanthresh = 0.2;
-matlabbatch{9}.spm.meeg.preproc.artefact.append = true;
-matlabbatch{9}.spm.meeg.preproc.artefact.methods.channels{1}.all = 'all';
-matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.threshold = 80;
-matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.excwin = 1000;
-matlabbatch{9}.spm.meeg.preproc.artefact.prefix = 'a';
-matlabbatch{10}.spm.meeg.averaging.average.D = {'C:\Users\Nastya\Documents\Freie Universitat\aefdfMspmeeg_subject1.mat'};
-matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.ks = 3;
-matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.bycondition = true;
-matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.savew = true;
-matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.removebad = false;
-matlabbatch{10}.spm.meeg.averaging.average.plv = false;
-matlabbatch{10}.spm.meeg.averaging.average.prefix = 'm';
+%--------------------------JOB SCRIPT FOR EEG PREPROCESSING--------------------------------
+
+
+
+% Number of Subj
+sub = 1;
+
+% Number of runs
+run = 1;
+
+% Here you clarify which Preprocessing Step you want to take
+% 1 = Convert
+% 2 = Montage
+% 3 = Prepare
+% 4 = High-Pass Filter
+% 5 = Downsample
+% 6 = Low-Pass Filter
+% 7 = Epoch
+% 8 = Artefacts
+% 8 = Averaging
+
+prepro_switch = [1,2,3,4]; 
+
+% give the prefix
+prefix = '';
+
+%% -------------------------- PreProcessing ------------------------------
+
+for s = 1:numel(sub)
+    for r = 1:numel(run)
+        for n = prepro_switch
+            switch n
+                case 1 % Convert
+                    matlabbatch{1}.spm.meeg.convert.dataset = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\subject1.bdf'};
+                    matlabbatch{1}.spm.meeg.convert.mode.continuous.readall = 1;
+                    matlabbatch{1}.spm.meeg.convert.channels{1}.chanfile = {'C:\Users\Nastya\Documents\Freie Universitat\channelselection.mat'};
+                    matlabbatch{1}.spm.meeg.convert.outfile = '';
+                    matlabbatch{1}.spm.meeg.convert.eventpadding = 0;
+                    matlabbatch{1}.spm.meeg.convert.blocksize = 3276800;
+                    matlabbatch{1}.spm.meeg.convert.checkboundary = 1;
+                    matlabbatch{1}.spm.meeg.convert.saveorigheader = 0;
+                    matlabbatch{1}.spm.meeg.convert.inputformat = 'autodetect';
+
+                case 2 % Montage
+
+                    matlabbatch{2}.spm.meeg.preproc.montage.D = {'C:\Users\Nastya\Documents\Freie Universitat\spmeeg_subject1.mat'};
+                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.montagefile = {'C:\Users\Nastya\Documents\Freie Universitat\avref_eog_montage.mat'};
+                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.keepothers = 0;
+                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.blocksize = 655360;
+                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.prefix = 'M';
+
+                case 3 % prepare
+
+                    matlabbatch{3}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
+                    matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.eegsens = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\sensors.pol'};
+                    matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.megmatch.nomatch = 1;
+
+                case 4 % Highpass Filter
+
+                    matlabbatch{4}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
+                    matlabbatch{4}.spm.meeg.preproc.filter.type = 'butterworth';
+                    matlabbatch{4}.spm.meeg.preproc.filter.band = 'high';
+                    matlabbatch{4}.spm.meeg.preproc.filter.freq = 0.1;
+                    matlabbatch{4}.spm.meeg.preproc.filter.dir = 'twopass';
+                    matlabbatch{4}.spm.meeg.preproc.filter.order = 5;
+                    matlabbatch{4}.spm.meeg.preproc.filter.prefix = 'f';
+
+                case 5 % Downsample
+
+                    matlabbatch{5}.spm.meeg.preproc.downsample.D = {'C:\Users\Nastya\Documents\Freie Universitat\fMspmeeg_subject1.mat'};
+                    matlabbatch{5}.spm.meeg.preproc.downsample.fsample_new = 200;
+                    matlabbatch{5}.spm.meeg.preproc.downsample.method = 'resample';
+                    matlabbatch{5}.spm.meeg.preproc.downsample.prefix = 'd';
+
+                case 6 % Lowpass filter
+
+                    matlabbatch{6}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\dfMspmeeg_subject1.mat'};
+                    matlabbatch{6}.spm.meeg.preproc.filter.type = 'butterworth';
+                    matlabbatch{6}.spm.meeg.preproc.filter.band = 'low';
+                    matlabbatch{6}.spm.meeg.preproc.filter.freq = 30;
+                    matlabbatch{6}.spm.meeg.preproc.filter.dir = 'twopass';
+                    matlabbatch{6}.spm.meeg.preproc.filter.order = 5;
+                    matlabbatch{6}.spm.meeg.preproc.filter.prefix = 'f';
+
+                case 7 % epoch
+
+                    matlabbatch{7}.spm.meeg.preproc.epoch.D = {'C:\Users\Nastya\Documents\Freie Universitat\fdfMspmeeg_subject1.mat'};
+                    matlabbatch{7}.spm.meeg.preproc.epoch.trialchoice.trlfile = {'C:\Users\Nastya\Documents\Freie Universitat\trialdef.mat'};
+                    matlabbatch{7}.spm.meeg.preproc.epoch.bc = 1;
+                    matlabbatch{7}.spm.meeg.preproc.epoch.eventpadding = 0;
+                    matlabbatch{7}.spm.meeg.preproc.epoch.prefix = 'e';
+
+                case 8 % artefacts
+
+                    matlabbatch{8}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
+                    matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.channels{1}.chan = 'A14';
+                    matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.status = 1;
+                    matlabbatch{9}.spm.meeg.preproc.artefact.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
+                    matlabbatch{9}.spm.meeg.preproc.artefact.mode = 'mark';
+                    matlabbatch{9}.spm.meeg.preproc.artefact.badchanthresh = 0.2;
+                    matlabbatch{9}.spm.meeg.preproc.artefact.append = true;
+                    matlabbatch{9}.spm.meeg.preproc.artefact.methods.channels{1}.all = 'all';
+                    matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.threshold = 80;
+                    matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.excwin = 1000;
+                    matlabbatch{9}.spm.meeg.preproc.artefact.prefix = 'a';
+
+                case 9 % averaging
+
+                    matlabbatch{10}.spm.meeg.averaging.average.D = {'C:\Users\Nastya\Documents\Freie Universitat\aefdfMspmeeg_subject1.mat'};
+                    matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.ks = 3;
+                    matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.bycondition = true;
+                    matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.savew = true;
+                    matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.removebad = false;
+                    matlabbatch{10}.spm.meeg.averaging.average.plv = false;
+                    matlabbatch{10}.spm.meeg.averaging.average.prefix = 'm';
+
+            end
+        end
+    end
+end
