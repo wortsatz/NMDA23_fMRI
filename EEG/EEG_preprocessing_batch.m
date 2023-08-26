5bbe8d9a5834d696d1659240cf369d7533badda1
Lisa Paulsen <72503842+wortsatz@users.noreply.github.com>
2023-08-26 07:45:56 +0200

    update SPM_EEG Master & Job

--- EEG/EEG_preprocessing_batch.m
+++ EEG/EEG_preprocessing_batch.m
@@ -11,19 +11,19 @@ whichComp=1;
 
 if whichComp==1
     spmPath='/Users/ttli/Documents/spm12/';
-    data_path='/Users/ttli/Documents/GitHub/NMDA23/98_fMRI_data';
+    data_path='/Users/ttli/Documents/GitHub/NMDA23/98_fMRI_data'/;
     project_path='/Users/ttli/Documents/GitHub/NMDA23/';
 elseif whichComp==2
     spmPath='/Users/kubrafatulla/Documents/spm12';
     data_path='/Users/kubrafatulla/Documents/fMRI/MoAEpilot/';
     project_path='/Users/kubrafatulla/Documents/GitHub/NMDA23/';
 elseif whichComp==3 
-    spmPath='/Users/lp1/Documents/spm12';
-    data_path='/Users/lp1/Nextcloud/FU/NMDA_data';
+    spmPath='/Users/lp1/Documents/spm12/';
+    data_path='/Users/lp1/Nextcloud/FU/NMDA_data/';
     project_path='/Users/lp1/Documents/GitHub/NMDA23/';
 else
-    spmPath='/Users/Nastya/программы/spm12';
-    data_path='/Users/Nastya/Documents/Freie Universitat/NMDA';
+    spmPath='/Users/Nastya/программы/spm12/';
+    data_path='/Users/Nastya/Documents/Freie Universitat/NMDA/';
     project_path='/Users/Nastya/Documents/GitHub/NMDA23/';
 end
 cd(data_path)
@@ -34,13 +34,6 @@ addpath(genpath(project_path))
 
 %% ------------------------- Preprocessing --------------------------------
 
-
-% List of open inputs
-nrun = 1; % enter the number of runs here
-preprocessing_jobfile = {'C:\Users\Nastya\Documents\Freie Universitat\EEG_preprocessing_batch_job.m'};
-jobs = preprocessing_jobfile;
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
+jobs = EEG_preprocessing_batch_job(data_path, spmPath);
 spm('defaults', 'EEG');
-spm_jobman('run', jobs, inputs{:});
+spm_jobman('run', jobs);
--- EEG/EEG_preprocessing_batch_job.m
+++ EEG/EEG_preprocessing_batch_job.m
@@ -1,5 +1,9 @@
+function [] = EEG_preprocessing_batch_job(datapath, spmPath)
+
 %--------------------------JOB SCRIPT FOR EEG PREPROCESSING--------------------------------
 
+% be aware that all the raw data and the created files should be stored in
+% one folder on your computer
 
 
 % Number of Subj
@@ -21,8 +25,14 @@ run = 1;
 
 prepro_switch = [1,2,3,4]; 
 
-% give the prefix
+% predetermine the filename
+bdf_filename = {'subject1.bdf'};
+spm_filename = {'spmeeg_subject1.mat'};
+
+
+% give the initial prefix
 prefix = '';
+
 
 %% -------------------------- PreProcessing ------------------------------
 
@@ -31,9 +41,9 @@ for s = 1:numel(sub)
         for n = prepro_switch
             switch n
                 case 1 % Convert
-                    matlabbatch{1}.spm.meeg.convert.dataset = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\subject1.bdf'};
+                    matlabbatch{1}.spm.meeg.convert.dataset = [ datapath prefix bdf_filename];
                     matlabbatch{1}.spm.meeg.convert.mode.continuous.readall = 1;
-                    matlabbatch{1}.spm.meeg.convert.channels{1}.chanfile = {'C:\Users\Nastya\Documents\Freie Universitat\channelselection.mat'};
+                    matlabbatch{1}.spm.meeg.convert.channels{1}.chanfile = [datapath 'channelselection.mat'];
                     matlabbatch{1}.spm.meeg.convert.outfile = '';
                     matlabbatch{1}.spm.meeg.convert.eventpadding = 0;
                     matlabbatch{1}.spm.meeg.convert.blocksize = 3276800;
@@ -43,59 +53,64 @@ for s = 1:numel(sub)
 
                 case 2 % Montage
 
-                    matlabbatch{2}.spm.meeg.preproc.montage.D = {'C:\Users\Nastya\Documents\Freie Universitat\spmeeg_subject1.mat'};
-                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.montagefile = {'C:\Users\Nastya\Documents\Freie Universitat\avref_eog_montage.mat'};
+                    matlabbatch{2}.spm.meeg.preproc.montage.D = [datapath prefix spm_filename];
+                    matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.montagefile = [datapath 'avref_eog_montage.mat'];
                     matlabbatch{2}.spm.meeg.preproc.montage.mode.write.montspec.montage.keepothers = 0;
                     matlabbatch{2}.spm.meeg.preproc.montage.mode.write.blocksize = 655360;
                     matlabbatch{2}.spm.meeg.preproc.montage.mode.write.prefix = 'M';
+                    prefix = ['M' prefix]; % updates the prefix to show that the Montage was done
 
                 case 3 % prepare
 
-                    matlabbatch{3}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
-                    matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.eegsens = {'C:\Users\Nastya\Documents\Freie Universitat\NMDA\sensors.pol'};
+                    matlabbatch{3}.spm.meeg.preproc.prepare.D = [datapath prefix spm_filename];
+                    matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.eegsens = [datapath 'sensors.pol'];
                     matlabbatch{3}.spm.meeg.preproc.prepare.task{1}.loadeegsens.megmatch.nomatch = 1;
 
                 case 4 % Highpass Filter
 
-                    matlabbatch{4}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\Mspmeeg_subject1.mat'};
+                    matlabbatch{4}.spm.meeg.preproc.filter.D = [datapath prefix spm_filename];
                     matlabbatch{4}.spm.meeg.preproc.filter.type = 'butterworth';
                     matlabbatch{4}.spm.meeg.preproc.filter.band = 'high';
                     matlabbatch{4}.spm.meeg.preproc.filter.freq = 0.1;
                     matlabbatch{4}.spm.meeg.preproc.filter.dir = 'twopass';
                     matlabbatch{4}.spm.meeg.preproc.filter.order = 5;
                     matlabbatch{4}.spm.meeg.preproc.filter.prefix = 'f';
+                    prefix = ['f' prefix]; % updates the prefix
 
                 case 5 % Downsample
 
-                    matlabbatch{5}.spm.meeg.preproc.downsample.D = {'C:\Users\Nastya\Documents\Freie Universitat\fMspmeeg_subject1.mat'};
+                    matlabbatch{5}.spm.meeg.preproc.downsample.D = [datapath prefix spm_filename];
                     matlabbatch{5}.spm.meeg.preproc.downsample.fsample_new = 200;
                     matlabbatch{5}.spm.meeg.preproc.downsample.method = 'resample';
                     matlabbatch{5}.spm.meeg.preproc.downsample.prefix = 'd';
+                    prefix = ['d' prefix]; % updates the prefix
 
                 case 6 % Lowpass filter
 
-                    matlabbatch{6}.spm.meeg.preproc.filter.D = {'C:\Users\Nastya\Documents\Freie Universitat\dfMspmeeg_subject1.mat'};
+                    matlabbatch{6}.spm.meeg.preproc.filter.D = [datapath prefix spm_filename];
                     matlabbatch{6}.spm.meeg.preproc.filter.type = 'butterworth';
                     matlabbatch{6}.spm.meeg.preproc.filter.band = 'low';
                     matlabbatch{6}.spm.meeg.preproc.filter.freq = 30;
                     matlabbatch{6}.spm.meeg.preproc.filter.dir = 'twopass';
                     matlabbatch{6}.spm.meeg.preproc.filter.order = 5;
                     matlabbatch{6}.spm.meeg.preproc.filter.prefix = 'f';
+                    prefix = ['f' prefix]; % updates the prefix
 
                 case 7 % epoch
 
-                    matlabbatch{7}.spm.meeg.preproc.epoch.D = {'C:\Users\Nastya\Documents\Freie Universitat\fdfMspmeeg_subject1.mat'};
-                    matlabbatch{7}.spm.meeg.preproc.epoch.trialchoice.trlfile = {'C:\Users\Nastya\Documents\Freie Universitat\trialdef.mat'};
+                    matlabbatch{7}.spm.meeg.preproc.epoch.D = [datapath prefix spm_filename];
+                    matlabbatch{7}.spm.meeg.preproc.epoch.trialchoice.trlfile = [datapath 'trialdef.mat'];
                     matlabbatch{7}.spm.meeg.preproc.epoch.bc = 1;
                     matlabbatch{7}.spm.meeg.preproc.epoch.eventpadding = 0;
                     matlabbatch{7}.spm.meeg.preproc.epoch.prefix = 'e';
+                    prefix = ['e' prefix]; % updates the prefix
 
                 case 8 % artefacts
 
-                    matlabbatch{8}.spm.meeg.preproc.prepare.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
+                    matlabbatch{8}.spm.meeg.preproc.prepare.D = [datapath prefix spm_filename];
                     matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.channels{1}.chan = 'A14';
                     matlabbatch{8}.spm.meeg.preproc.prepare.task{1}.setbadchan.status = 1;
-                    matlabbatch{9}.spm.meeg.preproc.artefact.D = {'C:\Users\Nastya\Documents\Freie Universitat\efdfMspmeeg_subject1.mat'};
+                    matlabbatch{9}.spm.meeg.preproc.artefact.D = [datapath prefix spm_filename];
                     matlabbatch{9}.spm.meeg.preproc.artefact.mode = 'mark';
                     matlabbatch{9}.spm.meeg.preproc.artefact.badchanthresh = 0.2;
                     matlabbatch{9}.spm.meeg.preproc.artefact.append = true;
@@ -103,16 +118,18 @@ for s = 1:numel(sub)
                     matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.threshold = 80;
                     matlabbatch{9}.spm.meeg.preproc.artefact.methods.fun.threshchan.excwin = 1000;
                     matlabbatch{9}.spm.meeg.preproc.artefact.prefix = 'a';
+                    prefix = ['a' prefix]; % updates the prefix
 
                 case 9 % averaging
 
-                    matlabbatch{10}.spm.meeg.averaging.average.D = {'C:\Users\Nastya\Documents\Freie Universitat\aefdfMspmeeg_subject1.mat'};
+                    matlabbatch{10}.spm.meeg.averaging.average.D = [datapath prefix spm_filename];
                     matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.ks = 3;
                     matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.bycondition = true;
                     matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.savew = true;
                     matlabbatch{10}.spm.meeg.averaging.average.userobust.robust.removebad = false;
                     matlabbatch{10}.spm.meeg.averaging.average.plv = false;
                     matlabbatch{10}.spm.meeg.averaging.average.prefix = 'm';
+                    prefix = ['m' prefix]; % updates the prefix
 
             end
         end
